8e8508c98033ca021a3017bf7c694bc18fb8838b
Chen Yumin <hello@chenyumin.com>
2017-03-16 16:17:40 +0000

    Use correlation to find wally

--- where_is_wally.m
+++ where_is_wally.m
@@ -23,33 +23,118 @@ clc;        % Clear command line
 clear all;  % Clear all variables
 close all;  % Close all sub-windows
 
-% Read image
+
+
+% Read images
 image = im2double(imread('Where.jpg'));
-%image = im2double(imread('Wally.png'));
-grayImage = rgb2gray(image); 
+template = im2double(imread('Wally.png'));
+
+[th, tw, ~] = size(template);
+
+% Use Correlation to find where Wally is
+[y, x] = correlation(image, template);
+
+figure;
+imshow(image);
+rectangle('Position', [x, y, tw, th], 'EdgeColor', 'r', 'LineWidth', 2);
+
+% Use color segmentation to find where Wally is 
+output = colorSegmentation(image);
+figure;
+imshow(output);
+end
+
+
+
+% ------------------------
+% Filter
+% ------------------------
+function output = filter(image, f)
+[height, width, ~] = size(image);
+output = image;
+for x = 2:width - 1
+    for y = 2: height - 1
+        for i = 1:3
+            % Dot multiply the "cookie" area by the filter matrix
+            filteredArea = image(y-1:y+1, x-1:x+1, i) .* f;
+            % Apply the sum to current position
+            output(y, x, i) = sum(filteredArea(:));
+        end
+    end
+end
+end
+
+% ------------------------
+% Correlation
+% ------------------------
+function [i_row, i_col] = correlation(image, template)
+
+imageGray = rgb2gray(image);
+templateGray = rgb2gray(template);
+
+[ih, iw] = size(imageGray);
+[th, tw] = size(templateGray);
+
+output = zeros(ih-th, iw-tw);
+
+for i = 1:ih-th
+    for j = 1:iw-tw
+        neighbourhood = imageGray(i:i+th-1, j:j+tw-1);
+        differences = abs(neighbourhood - templateGray);
+        output(i, j) = sum(differences(:))/(th*tw);
+        % Replace with actual correlation
+        %Find the best match in Output (min sum of differences or max
+        %correlation)
+        % Make this more efficient by ending the loop once found
+    end
+end
+
+%Using the 'rectangle' function, show where Wally is on the original image
+[~, I] = min(output(:));
+[i_row, i_col] = ind2sub(size(output), I);
+
+end
+
+
+
+
+
+
+% ------------------------
+% Color Segmentation
+% ------------------------
+function output = colorSegmentation(image)
 
 % Convert to HSV color space 
 imageHsv = rgb2hsv(image);
-h = imageHsv(:, :, 1);
+th = imageHsv(:, :, 1);
 s = imageHsv(:, :, 2);
 v = imageHsv(:, :, 3);
 
-roi = ((h < 0.075) | (h > 0.940)) & (v > 0.20) & (s < 0.9);
+roi = ((th < 0.075) | (th > 0.925)) & (v > 0.20) & (s < 0.9);
 
 r = image(:, :, 1);
 g = image(:, :, 2);
 b = image(:, :, 3);
+
+roi = bwareaopen(roi, 50);
 
 output = image;
 output(:, :, 1) = roi .* r;
 output(:, :, 2) = roi .* g;
 output(:, :, 3) = roi .* b;
 
+%structElement = strel('square', 3);
+%eroded = imerode(output, structElement);
 
-figure;
-imshow(output);
-figure;
+
+% Apply the Gaussian filter
+gauss = [-1 -2 -1;
+         0 0 0;
+         1 2 1]/4; % The Gaussian filter weighted sum matrix
+output = filter(output, gauss);
 
 end
+
 
 
