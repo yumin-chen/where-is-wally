b2d9ac663c6bcc6e7938f163f59b853673d7d2d5
Chen Yumin <hello@chenyumin.com>
2017-03-19 10:53:07 +0000

    Refine correlation efficiency

--- where_is_wally.m
+++ where_is_wally.m
@@ -15,8 +15,7 @@
 % -------------------------------------------------------------------------
 
 function where_is_wally
-% This is a hack that allows function definition in a script, this is used
-% so the code could be used for different images 
+% This is a hack that allows function definition in a script
 
 % Clear and clean enviroment
 clc;        % Clear command line
@@ -24,19 +23,16 @@ clear all;  % Clear all variables
 close all;  % Close all sub-windows
 
 
-
 % Read images
 image = im2double(imread('Where.jpg'));
 template = im2double(imread('Wally.png'));
 
-[th, tw, ~] = size(template);
-
 % Use Correlation to find where Wally is
-[y, x] = correlation(image, template);
+[x, y, w, h] = correlation(image, template);
 
 figure;
 imshow(image);
-rectangle('Position', [x, y, tw, th], 'EdgeColor', 'r', 'LineWidth', 2);
+rectangle('Position', [x, y, w, h], 'EdgeColor', 'r', 'LineWidth', 2);
 
 % Use color segmentation to find where Wally is 
 output = colorSegmentation(image);
@@ -47,56 +43,41 @@ end
 
 
 % ------------------------
-% Filter
+% Correlation Matching
 % ------------------------
-function output = filter(image, f)
-[height, width, ~] = size(image);
-output = image;
-for x = 2:width - 1
-    for y = 2: height - 1
-        for i = 1:3
-            % Dot multiply the "cookie" area by the filter matrix
-            filteredArea = image(y-1:y+1, x-1:x+1, i) .* f;
-            % Apply the sum to current position
-            output(y, x, i) = sum(filteredArea(:));
-        end
-    end
-end
-end
-
-% ------------------------
-% Correlation
-% ------------------------
-function [i_row, i_col] = correlation(image, template)
+function [x, y, w, h] = correlation(image, template)
 
 imageGray = rgb2gray(image);
 templateGray = rgb2gray(template);
 
 [ih, iw] = size(imageGray);
-[th, tw] = size(templateGray);
+[h, w] = size(templateGray);
 
-output = zeros(ih-th, iw-tw);
+% Use a 2-dimensional array to record the difference of each position
+diffs = zeros(ih-h, iw-w);
 
-for i = 1:ih-th
-    for j = 1:iw-tw
-        neighbourhood = imageGray(i:i+th-1, j:j+tw-1);
-        differences = abs(neighbourhood - templateGray);
-        output(i, j) = sum(differences(:))/(th*tw);
-        % Replace with actual correlation
-        %Find the best match in Output (min sum of differences or max
-        %correlation)
-        % Make this more efficient by ending the loop once found
+for i = 1:ih-h
+    for j = 1:iw-w
+        neighborhood = imageGray(i:i+h-1, j:j+w-1);
+        difference = abs(neighborhood - templateGray);
+        diffs(i, j) = sum(difference(:)) / (w*h);
+        
+        % If difference nearly zero, this is the exact match
+        if diffs(i, j) < 0.001
+            y = i;
+            x = j;
+            % Return the position found to improve efficiency
+            fprintf('An exact match is found. Return.')
+            return;
+        end
     end
 end
 
-%Using the 'rectangle' function, show where Wally is on the original image
-[~, I] = min(output(:));
-[i_row, i_col] = ind2sub(size(output), I);
+% Find the minimum difference value and its position
+[~, i] = min(diffs(:));
+[y, x] = ind2sub(size(diffs), i);
 
 end
-
-
-
 
 
 
@@ -107,11 +88,11 @@ function output = colorSegmentation(image)
 
 % Convert to HSV color space 
 imageHsv = rgb2hsv(image);
-th = imageHsv(:, :, 1);
+h = imageHsv(:, :, 1);
 s = imageHsv(:, :, 2);
 v = imageHsv(:, :, 3);
 
-roi = ((th < 0.075) | (th > 0.925)) & (v > 0.20) & (s < 0.9);
+roi = ((h < 0.075) | (h > 0.925)) & (v > 0.20) & (s < 0.9);
 
 r = image(:, :, 1);
 g = image(:, :, 2);
@@ -128,11 +109,7 @@ output(:, :, 3) = roi .* b;
 %eroded = imerode(output, structElement);
 
 
-% Apply the Gaussian filter
-gauss = [-1 -2 -1;
-         0 0 0;
-         1 2 1]/4; % The Gaussian filter weighted sum matrix
-output = filter(output, gauss);
+%output = imfilter(output, fspecial('prewitt'), 'replicate');
 
 end
 
